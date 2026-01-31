86db09ac6ec6d4c958c7186e25612f6c16cca49e
karlitomomoa <inbloomf3@gmail.com>
2026-01-31 17:42:04 +0300

    sdf

--- services/NotificationService/NotificationService.Api/Program.cs
+++ services/NotificationService/NotificationService.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 using System.Text;
@@ -115,7 +116,7 @@ app.Run();
 
 // NotificationHub implementation
 [Authorize]
-public class NotificationHub : Microsoft.AspNetCore.SignalR.Hub
+public class NotificationHub : Hub
 {
     private readonly ILogger<NotificationHub> _logger;
 
